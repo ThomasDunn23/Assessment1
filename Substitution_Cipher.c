bfece9568e2fff980ccc6a912f2d204003bcb675
Thomas  Dunn <c3328330@uon.edu.au>
2019-04-02 02:20:08 +0000

    Swapped Commenting around to test commit

--- Substitution_Cipher.c
+++ Substitution_Cipher.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+/* #include <stdio.h> // Will need to remove comment to run code 
 
 int main(void)
 {
     printf("hello, world\n");
-}
+} */ // REMOVE THIS COMMENT to run code 
