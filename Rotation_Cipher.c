1f3f3024041675cdb631b0deaf2078b3bd040925
Thomas  Dunn <c3328330@uon.edu.au>
2019-04-09 01:58:03 +0000

    Started Print statements

--- Rotation_Cipher.c
+++ Rotation_Cipher.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main()
-{char EncriptedMessage[99] ;// Announcing Array and Message
+{
+    char EncriptedMessage[99], chr ;// Announcing Array and Message
+    int x, shift  
 
- printf ("%c\n" , EncriptedMessage[1] + 1);
+    printf("Enter Message to Encript: %c\n", ) ; // Printing useable interface
+    scanf ("%c" , &chr);
+    printf("Enter Number of Shifts: %d\n" , shift) // User enters how many times they want to shift the cipher 
+    scanf ("%d" , &shift)
+    for ()
+
+
+
  }
  
