695c285bb150c1b9946cae319e2543befb7cead6
Thomas  Dunn <c3328330@uon.edu.au>
2019-04-09 03:40:04 +0000

    Completed Encription

--- Rotation_Cipher.c
+++ Rotation_Cipher.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
 
-int main()
+/*Before Running Code- Right Click on .c Program on left , Open in terminal , then type "./a.out" */
+
+int RotationEncription();
+int RotationDecription();
+
+
+
+int main() { 
+int inp ; 
+printf("Press 1 to open Encription:" ) ;
+scanf ("%d", &inp);
+switch (inp) {
+    case 1 : RotationEncription() ; 
+    break ;
+    case 2 : RotationDecription() ;
+    break ; 
+}}
+
+
+
+int RotationEncription()
 {
-    char EncriptedMessage[99], chr ;// Announcing Array and Message
-    int x, shift  
+    char Arr[99], chr ;  // Announcing Array and Message
+    int x, shift  ;
 
-    printf("Enter Message to Encript: %c\n", ) ; // Printing useable interface
+    printf("Enter Message to Encript:\n") ; // Printing useable interface
     scanf ("%c" , &chr);
-    printf("Enter Number of Shifts: %d\n" , shift) // User enters how many times they want to shift the cipher 
-    scanf ("%d" , &shift)
-    for ()
+    printf("Enter Number of Shifts in Encription: \n" ); // User enters how many times they want to shift the cipher 
+    scanf ("%d" , &shift);
+    
+    for (x = 0; Arr[x]!= '\0' ; x++ ) // \0 is the end of the array 
 
+    chr = Arr[x] ; 
+    if (chr >= 'a' && chr <= 'z') { // For letters between a and z on ASCII
+    chr = chr + shift ; 
+    if (chr > 'z') {
+        chr = chr - 'z' + 'a' - 1 ; } // Stops 
+  Arr[x]=chr ; 
+  }
+  else if (chr >= 'A' && chr<= 'Z') {
+      chr = chr + shift ; 
+      if (chr > 'Z') {
+          chr = chr - 'Z' + 'A' - 1 ; 
+      }
+      Arr[x]=chr ; 
+  }
+  printf ("Encription: %s", Arr);
+  return 0 ; }
+  
+  int RotationDecription() {
+      printf("Havent Completed this function Yet"); 
+      return 0 ; 
+  }
 
-
- }
  
