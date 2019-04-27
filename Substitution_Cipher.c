9ada16386f2bc13ac9eca4928161f4c172c5e569
Thomas  Dunn <c3328330@uon.edu.au>
2019-04-27 10:51:22 +0000

    Test for Overseas Save

--- Substitution_Cipher.c
+++ Substitution_Cipher.c
@@ -1,6 +1,19 @@
-/* #include <stdio.h> // Will need to remove comment to run code 
+ #include <stdio.h> // Will need to remove comment to run code 
 
-int main(void)
-{
-    printf("hello, world\n");
-} */ // REMOVE THIS COMMENT to run code 
+int SubstitutionEncription() ;
+int SubstitutionDecription() ;
+
+int SwitchSt () {
+int inp ; 
+printf("Press 1 to open Encription or 2 to open Decription:"); 
+scanf("%d", &inp); 
+switch (inp) {
+    case 1 : SubstitutionEncription() ;
+    break ; 
+    case 2 : SubstitutionDecription() ; 
+    break ; 
+}}
+
+int SubstitutionEncription
+}
+}  // REMOVE THIS COMMENT to run code 
