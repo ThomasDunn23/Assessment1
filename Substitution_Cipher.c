9875592895b9710cf3be54e7d8662d12fe2b4510
Thomas  Dunn <c3328330@uon.edu.au>
2019-04-28 10:51:25 +0000

    Finished Draft

--- Substitution_Cipher.c
+++ Substitution_Cipher.c
@@ -14,10 +14,10 @@ switch (inp) {
     break ; 
 }}
 
-    int SubstitutionEncryption(){
+    int SubstitutionEncryption(){ // Introduces Individual Function 
     char Arr[99];
     int x, i , key;
-    printf ("Enter Message to Encrypt:\n");
+    printf ("Enter Message to Encrypt:\n"); // User Interface
     scanf("%s" , Arr); 
     printf ("Key: \n"); 
     scanf("%d" , &key);
@@ -27,19 +27,40 @@ switch (inp) {
          Arr[x] = Arr [x] - 32 ; //Changing Lowercase Input to Uppercase by subsituting ASCII values 
     }}
     if (key=1) { 
-        char Key1[26] = "QAZWSXEDCRFVTGBYHNUJMIKOLP" ; 
-        for (i = 0; Arr[i] != '\0'; i++) {
+        char Key1[26] = "QAZWSXEDCRFVTGBYHNUJMIKOLP" ; // Creating Array and Encrytion Key 
+        for (i = 0; Arr[i] != '\0'; i++) { // Checks all numbers in Array 
             x = (int)Arr[i];
-        if (x>64 && x<91) {
-            x = x - 65;
-            Arr[i] = Key1[x] ; 
+        if (x>64 && x<91) { // ASCII characters 
+            x = x - 65; // Converting ASCII characters starting at 64 to Array starting at 0 
+            Arr[i] = Key1[x] ; // Creating Array as an Encryption 
     }}
     printf ("\nEncrypted Message: %s\n", Arr) ; }
     return 0 ; }
     
-    int SubstitutionDecryption (){
+    int SubstitutionDecryption (){ // Introduces Individual Function 
+    char Arr[99];
+    int x, i , key;
+    printf ("Enter Message to Decrypt:\n"); // User Interface
+    scanf("%s" , Arr); 
+    printf ("Key: \n"); 
+    scanf("%d" , &key);
+    printf ("Key: %d" , key ); 
+    for (x = 0; Arr[x]!= '\0' ; x++ ) { // Checks every number entered into Array [Since at the end of every array is \0]
+    if (Arr[x]>96 && Arr[x]< 123){
+         Arr[x] = Arr [x] - 32 ; //Changing Lowercase Input to Uppercase by subsituting ASCII values 
+    }}
+    if (key=1) { 
+        char Key1[26] = "BOIHGKNQVTWYURXZAJEMSLDFPC" ; // Encryted Array above Key decoded 
+        for (i = 0; Arr[i] != '\0'; i++) { // Checks all numbers in Array 
+            x = (int)Arr[i];
+        if (x>64 && x<91) { // ASCII characters 
+            x = x - 65; // Converting ASCII characters starting at 64 to Array starting at 0 
+            Arr[i] = Key1[x] ; // Creating Array as an Encryption 
+    }}
+    printf ("\nEncrypted Message: %s\n", Arr) ; }
+    return 0 ; }
         
-    }
+    
     
     
 
